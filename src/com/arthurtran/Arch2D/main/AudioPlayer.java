5a5a9b00bd5cb5fdd3f50e8fe4dc037de607f2a3
Arthur Tran <tran.art13@gmail.com>
2018-04-16 11:14:38 -0700

    Documentation

--- src/com/arthurtran/Arch2D/main/AudioPlayer.java
+++ src/com/arthurtran/Arch2D/main/AudioPlayer.java
@@ -60,7 +60,7 @@ public class AudioPlayer {
         FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         return (float) Math.pow(10f, gainControl.getValue() / 20f);
     }
-    
+
     public void setVolume(float volume) {
         if (volume < 0f || volume > 1f) {
             throw new IllegalArgumentException("Volume not valid: " + volume);
